4752dc061805690e4bf214135deba888fc0dcb41
DESKTOP-MC405SG\PcMax <fadi.a.ashy@gmail.com>
2020-07-24 14:35:06 +0300

    Ghost Sprinting
    Adding the Ghost Sprinting Game Mechanic

---
+++ Ghost Sprinting/Assets/Scripts/GhostSpriteSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSpriteSpawner : MonoBehaviour
+{
+    //[Replace with a pooling system to make the game faster]
+    [SerializeField] GameObject spritePrefab;
+
+    SpriteRenderer spriteRenderer;
+
+    //Time until spawning the next Ghost
+    [SerializeField] float time = 0.2f;
+    float currentTime = 0;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    //Called from the Update function in the movement Class either Player or Enemy
+    public void StartSpawningGhost()
+    {
+        //Add the time amount each Update
+        currentTime += Time.deltaTime;
+
+        //When the time is reach
+        if (currentTime >= time)
+        {
+            SpawnGhost();
+            //Reset Time
+            currentTime = 0;
+        }
+    }
+
+    //Put a ghost on the scene
+    void SpawnGhost()
+    {
+        //Start a new Ghost [Replace with a pooling system to make the game faster]
+        GameObject ghost = Instantiate(spritePrefab, transform.position, Quaternion.identity);
+
+        //Get the same sprite and flip direction 
+        ghost.GetComponent<SpriteRenderer>().sprite = spriteRenderer.sprite;
+        ghost.GetComponent<SpriteRenderer>().flipX = spriteRenderer.flipX;
+
+        //Destroy the Ghost after a while [Replace with a pooling system to make the game faster]
+        Destroy(ghost, 2f);
+    }
+}
---
+++ Ghost Sprinting/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    GhostSpriteSpawner ghostSpriteSpawner;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        ghostSpriteSpawner = GetComponent<GhostSpriteSpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    #region Movement
+    [SerializeField] float Speed = 5f; //Player Base Speed
+    float currentSpeed = 0;
+    float currentDirection = 0;
+
+    bool CanMove = true;
+
+    void Move()
+    {
+        float inputHor = Input.GetAxisRaw("Horizontal"); //Get Input
+
+        //Sprite Stuff
+        FlipSprite(inputHor);
+        ChangeSpeed(inputHor);
+
+        //To save the movement Direction after removing our Axis input
+        if (inputHor != 0)
+            currentDirection = inputHor;
+
+        if (CanMove)
+        {
+            transform.Translate(Vector3.right * currentDirection * currentSpeed * Time.deltaTime);
+
+            //Start Spawning Ghosts
+            ghostSpriteSpawner.StartSpawningGhost();
+
+            animator.SetBool("Run", true);
+        }
+        else
+            animator.SetBool("Run", false);
+    }
+    #endregion
+
+    //Flip the Player Sprite depend on the player direction
+    void FlipSprite(float inputDirection)
+    {
+        if (inputDirection > 0)
+            spriteRenderer.flipX = false;
+        else if (inputDirection < 0)
+            spriteRenderer.flipX = true;
+    }
+
+    //Ramp Speed
+    void ChangeSpeed(float inputDirection)
+    {
+        //if the movemont started
+        if (inputDirection != 0)
+        {
+            CanMove = true;
+
+            //Start Ramping Speed until reach the limit
+            currentSpeed += 0.75f;
+            if (currentSpeed >= Speed)
+                currentSpeed = Speed;
+        }
+        else
+        {
+            currentSpeed -= 0.5f;
+
+            //Stop moving when the speed go to 0
+            if (currentSpeed <= 0)
+            {
+                currentSpeed = 0;
+                CanMove = false;
+            }
+        }
+    }
+}
